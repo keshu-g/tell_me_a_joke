8b27d1da8f9255f016094391f4213dd845e673a0
keshu <96482872+keshuug@users.noreply.github.com>
2023-06-12 00:30:23 +0530

    Version_1

---
+++ script.js
@@ -0,0 +1,18 @@
+import data from './reddit_jokes.json' assert{ type: "json"};
+// console.log(data.length)
+// document.getElementsByTagName("h2")[0].innerHTML = "hello "
+window.make_joke = () => {
+    // console.log(123);
+    document.getElementsByTagName("h2")[0].innerHTML = "Loading"
+    let joke_index = Math.floor(Math.random() * data.length - 1)
+    let joke = data[joke_index]
+    
+    document.getElementsByTagName("h2")[0].innerHTML = joke.title
+    document.getElementsByTagName("p")[0].innerHTML = joke.body
+    return "done"
+}
+make_joke()
+// make_joke()
+
+// let btn = document.getElementsByTagName("a")[0]
+// btn.addEventListener('click',dojoke(1))
